ed9c700a9b2848e0f124740912371d16ab0f61a3
Shreyas H N <hn.shreyas70@gmail.com>
2020-06-29 01:09:22 +0530

    Payreceived page

---
+++ pages/Payreceived.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, Alert } from 'react-native';
+import Constants from 'expo-constants';
+
+
+
+export default function Payreceived() {
+
+
+
+    const componentDidMount = () => {
+        // Start counting when the page is loaded
+        this.timeoutHandle = setTimeout(() => {
+            // Add your logic for the transition
+            console.log("use redirect logic here!");
+        }, 2000);
+    };
+
+    const componentWillUnmount = () => {
+        clearTimeout(this.timeoutHandle); // This is just necessary in the case that the screen is closed before the timeout fires, otherwise it would cause a memory leak that would trigger the transition regardless, breaking the user experience.
+    }
+
+
+    componentDidMount();
+
+    componentWillUnmount();
+
+
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.animation} source={require('../assessts/success.gif')} />
+
+            <Text style={styles.message}> Payment successfull! </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#1a1f71',
+        padding: 8,
+    },
+    animation: {
+        height: 130,
+        width: 130,
+    },
+    message:
+        { color: "#fdbb0a", marginTop: 20, fontWeight: "bold", fontSize: 20 }
+});
+
+
