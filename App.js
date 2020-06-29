934941e07e2f8e3d373e790a32bd2a0c48672915
pradeep pallikila <pradeeppallikila1433@gmail.com>
2020-06-29 18:28:37 +0530

    flash screen added

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Button, TextInput, StyleSheet, Text, View } from 'react-native';
-import { Apploading } from 'expo';
-import { useScreens } from 'react-native-screens';
-import Constants from 'expo-constants';
+
 
 import Navigator from './navigation/Navigator';
 
